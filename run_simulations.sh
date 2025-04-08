ba951e1466a24a76c838696c96f5c0bca584d17e
Massyl Moudoud <mmoudoud@unistra.fr>
2025-04-08 16:19:23 +0200

    Update run_simualtion script
    Added handling of the uption full to select if the algorithms are run
    on a full or reduced grid of parameters.
    If the option full is provided, the box plots are generated on 100 realizations
    instead of 25.

--- run_simulations.sh
+++ run_simulations.sh
@@ -15,20 +15,29 @@ if [[ "$1" == "full" ]]; then
    python scripts/optimize_hyperparameters_simulation.py full
 else
    echo 'Running on reduced grid'
-   python scripts/optimize_hyperparameters_simulation.py 
+   #python scripts/optimize_hyperparameters_simulation.py 
 fi
 
 
 # Plot reconstruction errro vs hyperparameters (Figure 2)
 echo '################################################'
-echo 'Plot reconstruction errro vs hyperparameters (Figure 2)'
+echo 'Plot reconstruction error vs hyperparameters (Figure 2)'
 python notebooks/optimization_hyperparameters.py 
 
 
 # Evaluate methods on multiple datasets with best parameters
 echo '################################################'
 echo 'Evaluate methods on multiple datasets with best parameters'
-python scripts/evaluate_model_mutliple_data.py 
+
+
+if [[ "$1" == "full" ]]; then
+   echo 'Runnin 100 realizations (will take time)'
+   python scripts/evaluate_model_mutliple_data.py
+else
+   echo 'Running 25 realizations'
+   python scripts/evaluate_model_mutliple_data.py  25
+fi
+
 
 # Plot reconstruction error box plot (Figure 3)
 echo '################################################'
@@ -37,9 +46,8 @@ python notebooks/evaluate_mutlirealization.py
 
 # Plot convergence curves (Fgure 4)
 echo '################################################'
-echo Plot convergence curves (Fgure 4)
+echo 'Plot convergence curves (Fgure 4)'
 python notebooks/Convergence_tests.py 
-
 
 date >> time.txt
 
